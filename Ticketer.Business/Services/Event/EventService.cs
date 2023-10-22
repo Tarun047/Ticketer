834eaac99316ee4254bd00433b3b191c8faef790
Tarun047 <gtarun047@gmail.com>
2023-10-22 15:02:19 +0530

    Code cleanup

--- Ticketer.Business/Services/Event/EventService.cs
+++ Ticketer.Business/Services/Event/EventService.cs
@@ -2,13 +2,13 @@ using FluentValidation;
 using FluentValidation.Results;
 using OneOf;
 using Ticketer.Business.Models;
-using Ticketer.Business.Services.Errors;
 
 namespace Ticketer.Business.Services;
 
 public class EventService : BaseService<Event>
 {
     readonly IValidator<Event> eventValidator;
+
     public EventService(TicketerDbContext context, IValidator<Event> eventValidator) : base(context)
     {
         this.eventValidator = eventValidator;
@@ -26,8 +26,8 @@ public class EventService : BaseService<Event>
             Id = Guid.NewGuid(),
             Name = createEventRequest.Name,
             Time = createEventRequest.ScheduledAt.UtcDateTime
-        }; 
-        
+        };
+
         var result = await eventValidator.ValidateAsync(evt);
 
         if (result.IsValid)
